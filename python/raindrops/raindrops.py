895bd6edd09241aa506e45eed03ba41c12d52049
Bart Smykla <bartek@smykla.com>
2020-06-12 08:42:19 +0200

    python/raindrops - added second solution
    I did it in first way as I was focusing on performance, but exercism
    mentor suggested it's uneasy to maintain in the future (which I aggree)
    so I prepared also the second one
    
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -14,3 +14,13 @@ def convert(number):
             return result
     else:
         return str(number)
+
+def convert(number):
+    drops = {
+        3: "Pling",
+        5: "Plang",
+        7: "Plong",
+    }
+
+    return ''.join(value for key, value in drops.items() if number % key == 0) \
+        or str(number)
