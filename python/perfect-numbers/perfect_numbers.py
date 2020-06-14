15e9b86c99c54c30cf69d292d826787171141015
Bart Smykla <bartek@smykla.com>
2020-06-14 09:57:51 +0200

    Added solution to python/perfect_number exercise
    Signed-off-by: Bart Smykla <bartek@smykla.com>

---
+++ python/perfect-numbers/perfect_numbers.py
@@ -0,0 +1,32 @@
+from functools import reduce
+from math import sqrt, copysign
+
+
+def _get_factors(n):
+    step = 2 if n % 2 else 1
+    return set(reduce(list.__add__,
+                      ([i, n//i] for i in range(1, int(sqrt(n))+1, step) if not n % i)))
+
+
+def _cmp(a, b):
+    '''
+    Compare two numbers and return -1 if 'b' is smaller, 0 if equal and 1
+    if greater than `a`
+    '''
+    return (a > b) - (a < b)
+
+
+Classification = {
+    -1: "deficient",
+    0: "perfect",
+    1: "abundant",
+}
+
+
+def classify(number):
+    if number < 1:
+        raise ValueError("Passed argument is not a natural number")
+
+    aliquot_sum = sum(_get_factors(number)) - number
+
+    return Classification[_cmp(aliquot_sum, number)]
